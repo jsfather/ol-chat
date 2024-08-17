44e1900d8a7f04699a6ca193231cfdb51f47d372
jsfather <keyvanmatinrad@outlook.com>
2024-08-17 14:30:31 +0330

    changed some types to not be required

--- types/chat.ts
+++ types/chat.ts
@@ -1,18 +1,18 @@
 export interface Chat {
-    model: string,
-    created_at: Date,
+    model?: string,
+    created_at?: Date,
     message: {
         role: string,
         content: string
     },
-    done_reason: string,
-    done: boolean,
-    total_duration: number,
-    load_duration: number,
-    prompt_eval_count: number,
-    prompt_eval_duration: number,
-    eval_count: number,
-    eval_duration: number
+    done_reason?: string,
+    done?: boolean,
+    total_duration?: number,
+    load_duration?: number,
+    prompt_eval_count?: number,
+    prompt_eval_duration?: number,
+    eval_count?: number,
+    eval_duration?: number
 }
 
 export interface ChatState {
