62eb91a5cad40ade70a41ea5bb0c8ab031981648
jsfather <keyvanmatinrad@outlook.com>
2024-08-19 17:45:37 +0330

    added new skeleton

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,11 @@
 <template>
-  <NuxtPage/>
+  <div class="flex h-screen">
+    <app-sidebar />
+    <main class="flex-1 bg-slate-800 px-3 py-2">
+      <NuxtPage />
+    </main>
+  </div>
 </template>
+
+<script setup lang="ts">
+</script>
