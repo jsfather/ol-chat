52ff2e6993b8607f816a1fe3a8c1397e24ed0fc9
jsfather <keyvanmatinrad@outlook.com>
2024-08-19 17:45:19 +0330

    added new type

---
+++ types/tag.ts
@@ -0,0 +1,15 @@
+export interface Tag {
+    name: string,
+    model: string,
+    modified_at: Date,
+    size: number,
+    digest: string,
+    details: {
+        parent_model: string,
+        format: string,
+        family: string,
+        families: string[],
+        "parameter_size": string,
+        "quantization_level": string
+    }
+}
