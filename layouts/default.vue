ea18b5ec7cf5f407fd7f12db51a206438aad6d7a
jsfather <keyvanmatinrad@outlook.com>
2024-08-20 22:26:23 +0330

    paddings fixed

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex h-screen">
     <app-sidebar />
-    <main class="flex-1 bg-slate-800 px-3 py-2">
+    <main class="flex-1 bg-slate-800">
       <NuxtPage />
     </main>
   </div>
