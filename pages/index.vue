54e7738907007492a52c3aa70fc936083e3dcc75
jsfather <keyvanmatinrad@outlook.com>
2024-08-20 17:53:50 +0330

    chat layout fixed

--- pages/index.vue
+++ pages/index.vue
@@ -18,27 +18,31 @@ async function sendMessage() {
 }
 </script>
 <template>
-  <app-popover v-model="selectedTag" :items="tagStore.tags"/>
-
-  <form class="fixed bottom-6 left-0 right-0 w-1/2 mx-auto" @submit.prevent="sendMessage">
-    <div class="relative">
-      <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-        <Icon name="material-symbols:attach-file" size="25" class="text-gray-500"/>
+  <div>
+    <app-popover v-model="selectedTag" :items="tagStore.tags"/>
+    <div class="p-4 w-full flex flex-col justify-between h-[calc(100vh-60px)]">
+      <div>
+        <div v-for="item in chatStore.chatHistory" >{{ item.content }}</div>
       </div>
-      <input v-model="message" type="search" id="default-search"
-             class="block w-full p-4 ps-10 text-gray-200 rounded-3xl bg-gray-700 focus:outline-none"
-             :placeholder="`Ask ${selectedTag?.details?.family} anything...`" required :disabled="chatStore.isLoading"/>
-      <button type="submit"
-              class="absolute end-2.5 bottom-3 bg-white disabled:bg-gray-500 rounded-full p-2.5 text-center inline-flex items-center"
-              :disabled="message === '' || chatStore.isLoading">
-        <Icon v-if="!chatStore.isLoading" name="material-symbols:arrow-upward" size="15"></Icon>
-        <Icon v-else name="material-symbols:square-rounded" size="15" class="text-gray-300"></Icon>
-      </button>
+      <form class="w-1/2 mx-auto" @submit.prevent="sendMessage">
+        <div class="relative">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <Icon name="material-symbols:attach-file" size="25" class="text-gray-500"/>
+          </div>
+          <input v-model="message" type="search" id="default-search"
+                 class="block w-full p-4 ps-10 text-gray-200 rounded-3xl bg-gray-700 focus:outline-none"
+                 :placeholder="`Ask ${selectedTag?.details?.family} anything...`" required
+                 :disabled="chatStore.isLoading"/>
+          <button type="submit"
+                  class="absolute end-2.5 bottom-3 bg-white disabled:bg-gray-500 rounded-full p-2.5 text-center inline-flex items-center"
+                  :disabled="message === '' || chatStore.isLoading">
+            <Icon v-if="!chatStore.isLoading" name="material-symbols:arrow-upward" size="15"></Icon>
+            <Icon v-else name="material-symbols:square-rounded" size="15" class="text-gray-300"></Icon>
+          </button>
+        </div>
+      </form>
     </div>
-  </form>
 
-  <div class="p-4 w-full flex flex-col h-full">
-      <div v-for="item in chatStore.chatHistory">{{item.content}}</div>
+
   </div>
-
 </template>
