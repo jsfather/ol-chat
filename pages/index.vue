26c4b9c7c86f610977c4a61c4241ae5875215595
jsfather <keyvanmatinrad@outlook.com>
2024-08-20 10:59:02 +0330

    new tagStore added

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
-<template>
-  <app-popover/>
-</template>
 <script setup lang="ts">
+const tagStore = useTagStore()
+
+await callOnce(tagStore.fetchAll)
+
+const selectedTag = ref(tagStore.selectedTag)
+
+watch(selectedTag, (newValue) => {
+  tagStore.setSelectedTag(newValue)
+});
 </script>
+<template>
+  <app-popover v-model="selectedTag" :items="tagStore.tags"/>
+</template>
