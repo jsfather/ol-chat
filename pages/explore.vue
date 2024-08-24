9ee6231aabca28b8f866ea7f511814188591d5a9
jsfather <keyvanmatinrad@outlook.com>
2024-08-24 13:56:06 +0330

    added more fakerjs to component for tests

--- pages/explore.vue
+++ pages/explore.vue
@@ -3,23 +3,19 @@ import {faker} from '@faker-js/faker';
 
 const toastStore = useToastStore()
 
-const types = ["info", "error", "success", "warning"];
+import {SimpleFaker} from '@faker-js/faker';
 
-function getRandomType() {
-  const randomIndex = Math.floor(Math.random() * types.length);
-  return types[randomIndex];
-}
+const customSimpleFaker = new SimpleFaker();
 
 function createFakeToast() {
   toastStore.addToast({
-    message: faker.helpers.fake(
-        'Hello {{person.prefix}} {{person.lastName}}, how are you today?'
-    ), type: getRandomType(), duration: (Math.random() * (20 - 5) + 5) * 1000
+    message: faker.person.firstName() + ' loves ' + faker.music.songName(),
+    type: customSimpleFaker.helpers.arrayElement(["info", "error", "success", "warning"]),
+    duration: faker.number.int({min: 3, max: 20}) * 1000
   })
 }
 
 </script>
-
 <template>
   <button type="button"
           class="text-white m-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
@@ -27,7 +23,5 @@ function createFakeToast() {
     Show fake toast
   </button>
 </template>
-
 <style scoped>
-
 </style>
