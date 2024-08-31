a6aed3cfafddeacb5bfdde987f322ec02e6c8f90
jsfather <keyvanmatinrad@outloook.com>
2024-08-31 10:13:06 +0330

    fixed functionality

--- stores/tag.ts
+++ stores/tag.ts
@@ -1,4 +1,5 @@
 import type {Tag} from "@/types/tag"
+import type {Error} from "~/types/error";
 
 export const useTagStore = defineStore('tagStore', {
     state: () => ({
@@ -7,9 +8,14 @@ export const useTagStore = defineStore('tagStore', {
     }),
     actions: {
         async fetchAll() {
-            const data = await $fetch<{ models: Tag[] }>('http://localhost:11434/api/tags')
-            this.tags = data.models
-            this.selectedTag = data.models[0]
+            const toastStore = useToastStore()
+            try {
+                const data = await $fetch<{ models: Tag[] }>('http://localhost:11434/api/tags')
+                this.tags = data.models
+                this.selectedTag = data.models[0]
+            } catch (error: Error | any) {
+                toastStore.addToast({message: error, type: 'error', duration: 'permanent'})
+            }
         },
         setSelectedTag(tag: Tag) {
             this.selectedTag = tag
