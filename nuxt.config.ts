cd586d52c88044f0644703bf23a879f582e2f47d
jsfather <keyvanmatinrad@outlook.com>
2024-08-31 16:40:44 +0330

    loadash added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-08-19',
     ssr: false,
-    modules: ['@nuxtjs/tailwindcss', 'nuxt-headlessui', "@nuxt/icon", "@pinia/nuxt", '@formkit/auto-animate/nuxt'],
+    modules: ['@nuxtjs/tailwindcss', 'nuxt-headlessui', "@nuxt/icon", "@pinia/nuxt", '@formkit/auto-animate/nuxt', "nuxt-lodash"],
     css: ['@/assets/css/main.css'],
     headlessui: {
         prefix: 'Headless'
