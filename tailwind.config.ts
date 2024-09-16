0da94f2944924f6b14c84373534fc97a80a566da
jsfather <keyvanmatinrad@outlook.com>
2024-09-16 15:08:38 +0330

    new primary color added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,13 @@
 import type {Config} from 'tailwindcss'
 
 export default <Partial<Config>>{
+    theme: {
+        extend: {
+            colors: {
+                primary: '#18C3F8'
+            }
+        }
+    },
     plugins: [
         require('tailwind-scrollbar'),
     ]
