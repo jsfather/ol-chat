0334164ac04c86a6538f1a596fb51ade41f324ed
jsfather <keyvanmatinrad@outlook.com>
2024-09-17 10:15:11 +0330

    new AppChips.vue component added

---
+++ components/AppChips.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div
+      class="rounded-md py-0.5 px-2.5 border border-transparent text-sm text-gray-400 transition-all text-nowrap bg-slate-900 select-none">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
